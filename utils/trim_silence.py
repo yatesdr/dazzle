43688a8227aec24c59c39bc5e468cd42b12ac296
derek <derek.yates@live.com>
2024-04-22 14:28:01 -0400

    fixed silence trimmer

--- utils/trim_silence.py
+++ utils/trim_silence.py
@@ -6,12 +6,13 @@ trim_leading_silence = lambda x: x[detect_leading_silence(x) :]
 trim_trailing_silence = lambda x: trim_leading_silence(x.reverse()).reverse()
 strip_silence = lambda x: trim_trailing_silence(trim_leading_silence(x))
 
-path = "~/dazzle/media/wav/playlist2"
+path = "/Users/derek/Documents/Code/DazzleDizzle/media/wav/playlist2"
+
 for item in os.listdir(path):
-    print("Processing ",item)
-    if os.path.isfile(item) and item.split(".").upper()=="WAV":
-        sound: AudioSegment = AudioSegment.from_file(item)
+    if os.path.isfile(f"{path}/{item}") and item.split(".")[-1].upper()=="WAV":
+        print("Processing ",item)
+        sound: AudioSegment = AudioSegment.from_file(f"{path}/{item}")
         stripped: AudioSegment = strip_silence(sound)
-        sound.export(item,format="wav")
+        stripped.export(f"{path}/{item}",format="wav")
     print("Done.")
 
